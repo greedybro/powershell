66fec3b5c8d7ab3c9513ee516d51f5c422ac7dd3
greedybro <locks.survivor@gmail.com>
2019-01-17 19:37:29 +0100

    Remove specific path

--- cp_shortcut/cp_shortcut.ps1
+++ cp_shortcut/cp_shortcut.ps1
@@ -1,4 +1,4 @@
-﻿Get-ChildItem "\\ad\dep$\Config\shortcut" 
+﻿Get-ChildItem "C:\Path\shortcut" 
 
-Copy-Item "\\ad\dep`$\Config\shortcut\Google Chrome.lnk" -Destination "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\"
+Copy-Item "C:\Path\shortcut\Google Chrome.lnk" -Destination "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\"
 
